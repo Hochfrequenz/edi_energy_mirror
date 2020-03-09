6b8f2de9459ab58947cd861ab2c8f5a7aedf5485
Konstantin Klein <konstantin.klein@hochfrequenz.de>
2020-03-09 09:31:07 +0100

    add batch file

---
+++ delete_and_download.bat
@@ -0,0 +1,15 @@
+C:
+cd C:/Users/Administrator/Documents/edi_energy_mirror/
+git pull
+rm edi_energy_de/*.html
+rm edi_energy_de/future/*.pdf
+rm edi_energy_de/current/*.pdf
+rm edi_energy_de/past/*.pdf
+
+python download_and_post_process.py
+git add edi_energy_de/*.html
+git add edi_energy_de/future/*.pdf
+git add edi_energy_de/current/*.pdf
+git add edi_energy_de/past/*.pdf
+git commit -m "edi energy mirror cronjob"
+git push
