20760ac1d8f0c52e68bacbc74ad96bdc84827346
Konstantin Klein <konstantin.klein@hochfrequenz.de>
2020-03-09 11:07:40 +0100

    navigate into directories before deletion

--- delete_and_download.bat
+++ delete_and_download.bat
@@ -1,10 +1,13 @@
 C:
 cd C:/Users/Administrator/Documents/edi_energy_mirror/
 git pull
-del edi_energy_de/future/*.pdf
-del edi_energy_de/current/*.pdf
-del edi_energy_de/past/*.pdf
-
+cd edi_energy_de/future
+del *.pdf
+cd ../current/
+del *.pdf
+cd ../past/
+del *.pdf
+cd ../../
 python download_and_post_process.py
 git add edi_energy_de/*.html
 git add --no-ignore-removal edi_energy_de/future/*.pdf
