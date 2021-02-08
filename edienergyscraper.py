3ef3605bc340d484329f677178127a597e16d978
konstantin <konstantin.klein@hochfrequenz.de>
2021-02-08 11:59:57 +0100

    more tests

--- edienergyscraper.py
+++ edienergyscraper.py
@@ -2,6 +2,9 @@
 A module to scrape data from edi-energy.de.
 """
 from pathlib import Path
+from random import randint
+from time import sleep
+from typing import Callable
 
 import requests
 from bs4 import BeautifulSoup, Comment
@@ -12,12 +15,17 @@ class EdiEnergyScraper:
     A class that uses beautiful soup to extract and download data from edi-energy.de.
     """
 
-    def __init__(self, root_url: str = "https://www.edi-energy.de", directory: Path = Path("edi_energy_de")):
+    def __init__(self, root_url: str = "https://www.edi-energy.de", directory: Path = Path("edi_energy_de"),
+                 waiter: Callable = lambda: sleep(randint(1, 10))):
+        """
+        Initialize the Scaper by providing the URL, a path to save the files to and a function that prevents DOS.
+        """
         self._root_url = root_url.strip()
         if self._root_url.endswith("/"):
-            # remove trailing slashes
+            # remove trailing slash if any
             self._root_url = self._root_url[:-1]
         self._root_dir = directory
+        self._dos_waiter = waiter
 
     def get_index(self) -> BeautifulSoup:
         """
@@ -27,6 +35,7 @@ class EdiEnergyScraper:
         root_response = requests.get(self._root_url)
         root_soup = BeautifulSoup(root_response.content, "html.parser")
         EdiEnergyScraper.remove_comments(root_soup)
+        self._dos_waiter()
         return root_soup
 
     @staticmethod
@@ -36,6 +45,18 @@ class EdiEnergyScraper:
         """
         for html_comment in soup.findAll(text=lambda text: isinstance(text, Comment)):
             html_comment.extract()
+
+    def get_documents_page_link(self, soup) -> str:
+        """
+        Extracts the links for the "Dokumente" from a given index soup.
+        """
+        documents_link = soup.find("a", {"title": "Dokumente"})
+        if not documents_link:
+            raise ValueError("The soup did not contain a link called \"Dokumente\".")
+        documents_url = documents_link.attrs["href"]
+        if not documents_url.startswith("http"):
+            documents_url = self._root_url + documents_link.attrs["href"]
+        return documents_url
 
     def mirror(self):
         """
--- unittests/test_edienergyscraper.py
+++ unittests/test_edienergyscraper.py
@@ -1,4 +1,15 @@
+import time
+
+import pytest
+
 from edienergyscraper import EdiEnergyScraper
+
+
+def fast_waiter():
+    """
+    a helper method to prevent actual DOS waiting (up to 3 seconds per call.)
+    """
+    time.sleep(0)
 
 
 class TestEdiEnergyScraper:
@@ -13,8 +24,34 @@ class TestEdiEnergyScraper:
         instance = EdiEnergyScraper(root_url="https://my_url.de/")
         assert not instance._root_url.endswith("/")
 
-    def test_index_retrieval(self):
+    @pytest.mark.datafiles(
+        "testfiles/index_20210208.html",
+    )
+    def test_index_retrieval(self, requests_mock):
         """
         Tests that the landing page is downloaded correctly
         """
-        pass
+        with open("testfiles/index_20210208.html", "r", encoding="utf8") as infile:
+            response_body = infile.read()
+        assert "<!--" in response_body
+        requests_mock.get("https://www.my_root_url.test", text=response_body)
+        ees = EdiEnergyScraper("https://www.my_root_url.test", waiter=fast_waiter)
+        actual_soup = ees.get_index()
+        actual_html = actual_soup.prettify()
+        assert "<!--" not in actual_html, "comments should be ignored/removed"
+        assert "Startseite: BDEW Forum Datenformate" in actual_html, "content should be returned"
+
+    @pytest.mark.datafiles(
+        "testfiles/index_20210208.html",
+    )
+    def test_dokumente_link(self, requests_mock):
+        """
+        Tests that the "Dokumente" link is extracted from the downloaded landing page.
+        """
+        with open("testfiles/index_20210208.html", "r", encoding="utf8") as infile:
+            response_body = infile.read()
+        assert "<!--" in response_body
+        requests_mock.get("https://www.edi-energy.de", text=response_body)
+        ees = EdiEnergyScraper("https://www.edi-energy.de", waiter=fast_waiter)
+        actual_link = ees.get_documents_page_link(ees.get_index())
+        assert actual_link == "https://www.edi-energy.de/index.php?id=38"
