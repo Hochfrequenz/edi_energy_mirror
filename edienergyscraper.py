ff1b68067cf4e6f1d0bf0e47474283bbce1291ff
hf-kklein <konstantin.klein@hochfrequenz.de>
2021-02-11 18:17:00 +0100

    Setup a Stable Code Basis using Tox, Pylint and Black (#5)
    * add requirements.in
    
    * pip-compile requirements.in
    
    * add tox.ini
    
    * add gitignore on root level
    
    * add a GH action for black
    
    * add requests to requirements
    
    * add a linter gh action
    
    * gitignore .tox directory
    
    * apply black formatter to only python file
    
    * apply linter hints
    
    * introduce a class + unittests
    
    * update lint command
    
    * more tests
    
    * a whole lot of test cases
    
    * add pytest-mock
    
    * add a pylint rc
    
    * a final test
    
    * ignore stuff
    
    * black
    
    * 🔧 Add VS Code settings
    
    set python interpreter to dev environment and activate pylint
    
    * 🐛 Fix paths to test files
    
    pytest datafiles copies the given files into a temporary folder.
    You have to add the datafiles argument to the test function and use it to define the final path in your test function
    The lookup for the files to copy, starts at the root directory of your project.
    So we had to replace `"testfiles/example_ahb.pdf",` with `"./unittests/testfiles/example_ahb.pdf",`
    You can also leave out the `./` but it makes it more clear for the reader, where the path starts
    
    * remove unnecessary try/catch
    
    * use realistic values
    
    inclusive end dates are for beginners
    
    * Add a link to the BS4 documentation in docstring
    
    Co-authored-by: hf-aschloegl <73470827+hf-aschloegl@users.noreply.github.com>
    
    * inline comment
    
    Co-authored-by: hf-aschloegl <73470827+hf-aschloegl@users.noreply.github.com>
    
    * Upper case at beginnning of docstring sentece
    
    Co-authored-by: hf-aschloegl <73470827+hf-aschloegl@users.noreply.github.com>
    
    * add a comment about "a"
    
    * enhance comment: the file path starts with the root dir
    
    * introduce an aenum for epoch
    
    Annika is right. the stringly typed thing won't help anyone
    linter
    
    * rename constructor argument from "waiter" to "dos_waiter"
    
    * rename directory argument
    
    * re-add download_and_post_process.py
    
    Co-authored-by: Kevin <kevin.krechan@hochfrequenz.de>
    Co-authored-by: hf-aschloegl <73470827+hf-aschloegl@users.noreply.github.com>

---
+++ edienergyscraper.py
@@ -0,0 +1,219 @@
+"""
+A module to scrape data from edi-energy.de.
+"""
+import datetime
+import re
+from pathlib import Path
+from random import randint
+from time import sleep
+from typing import Callable, Dict
+
+import aenum
+import requests
+from bs4 import BeautifulSoup, Comment
+
+
+class Epoch(aenum.Enum):  # pylint: disable=too-few-public-methods
+    """
+    An Epoch describes the time range in which documents are valid.
+    """
+
+    _init_ = "value string"
+    PAST = 1, "past"  # documents that are not valid anymore an have been archived
+    CURRENT = (
+        2,
+        "current",
+    )  # documents that are currently valid valid_from <= now < valid_to
+    FUTURE = (
+        3,
+        "future",
+    )  # documents that will become valid in the future (most likely with the next format version)
+
+    def __str__(self):
+        return self.string
+
+
+class EdiEnergyScraper:
+    """
+    A class that uses beautiful soup to extract and download data from edi-energy.de.
+    Beautiful soup is a library that makes it easy to scrape information from web pages:
+    https://pypi.org/project/beautifulsoup4/
+    """
+
+    def __init__(
+        self,
+        root_url: str = "https://www.edi-energy.de",
+        path_to_mirror_directory: Path = Path("edi_energy_de"),
+        # HTML and PDF files will be stored relative to this
+        dos_waiter: Callable = lambda: sleep(randint(1, 10)),
+    ):
+        """
+        Initialize the Scaper by providing the URL, a path to save the files to and a function that prevents DOS.
+        """
+        self._root_url = root_url.strip()
+        if self._root_url.endswith("/"):
+            # remove trailing slash if any
+            self._root_url = self._root_url[:-1]
+        self._root_dir = path_to_mirror_directory
+        self._dos_waiter = dos_waiter
+
+    def _get_soup(self, url: str) -> BeautifulSoup:
+        """
+        Downloads the given absolute URL, parses it as html, removes the comments and returns the soup.
+        """
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        EdiEnergyScraper.remove_comments(soup)
+        self._dos_waiter()  # <-- DOS protection, usually a blocking method (e.g. time.sleep(...))
+        return soup
+
+    def _download_and_save_pdf(self, epoch: Epoch, file_name: str, link: str) -> bytes:
+        """
+        Downloads a PDF file from a given link and stores it under the file name in a folder that has the same name
+        as the directory.
+        Returns the PDF.
+        """
+        if not file_name.endswith(".pdf"):
+            raise ValueError(
+                f"This method is thought to save pdf files but the filename was {file_name}"
+            )
+        if "/" in file_name:
+            raise ValueError(f"file names must not contain slashes: '{file_name}'")
+        if not link.startswith("http"):
+            link = f"{self._root_url}/{link.strip('/')}"  # remove trailing slashes from relative link
+        response = requests.get(link)
+        file_path = Path(self._root_dir).joinpath(
+            f"{epoch}/{file_name}"  # e.g "{root_dir}/future/ahbmabis_99991231_20210401.pdf"
+        )
+        with open(file_path, "wb+") as outfile:  # pdfs are written as binaries
+            outfile.write(response.content)
+        return response.content
+
+    def get_index(self) -> BeautifulSoup:
+        """
+        Downloads the root url and returns the soup.
+        """
+        # As the landing page is usually called "index.html/php/..." this method is named index.
+        return self._get_soup(self._root_url)
+
+    @staticmethod
+    def remove_comments(soup):
+        """
+        Removes thes HTML comments from the given soup.
+        """
+        for html_comment in soup.findAll(text=lambda text: isinstance(text, Comment)):
+            html_comment.extract()
+
+    def get_documents_page_link(self, index_soup) -> str:
+        """
+        Extracts the links for the "Dokumente" from a given index soup.
+        """
+        # HTML links look like this <a href="url">...</a>; That's why we search for "a"
+        documents_link = index_soup.find("a", {"title": "Dokumente"})
+        if not documents_link:
+            raise ValueError('The soup did not contain a link called "Dokumente".')
+        documents_url = documents_link.attrs["href"]
+        if not documents_url.startswith("http"):
+            documents_url = self._root_url + documents_link.attrs["href"]
+        return documents_url
+
+    # a dictionary that maps link titles to short names.
+    _docs_texts: Dict[str, Epoch] = {
+        "Aktuell gültige Dokumente": Epoch.CURRENT,
+        "Zukünftig gültige Dokumente": Epoch.FUTURE,
+        "Archivierte Dokumente": Epoch.PAST,
+    }
+
+    @staticmethod
+    def get_epoch_links(document_soup) -> Dict[Epoch, str]:
+        """
+        Extract the links to
+        * "Aktuell gültige Dokumente"
+        * "Zukünftig gültige Dokumente"
+        * "Archivierte Dokumente"
+        from the "Dokumente" sub page soup.
+        """
+        result: Dict[Epoch, str] = dict()
+        for doc_text in EdiEnergyScraper._docs_texts:
+            result[EdiEnergyScraper._docs_texts[doc_text]] = document_soup.find(
+                "a", string=re.compile(r"\s*" + doc_text + r"\s*")
+            ).attrs["href"]
+        # result now looks like this:
+        # { "past": "link_to_vergangene_dokumente.html", "current": "link_to_active_docs.html", "future": ...}
+        # see the unittest
+        return result
+
+    @staticmethod
+    def get_epoch_file_map(epoch_soup: BeautifulSoup) -> Dict[str, str]:
+        """
+        Extracts a dictionary from the epoch soup (e.g. soup of "future.html") that maps filenames as keys
+        (e.g. "APERAKCONTRLAHB2.3h_99993112_20210104.pdf") to URLs of the documents as value.
+        """
+        download_table = epoch_soup.find(
+            "table", {"class": "table table-responsive table-condensed"}
+        )  # a table that contains all the documents
+        result: Dict[str, str] = dict()
+        for table_row in download_table.find_all("tr"):
+            table_cells = list(table_row.find_all("td"))
+            if len(table_cells) < 4:
+                # Not all the rows in the table contain 4 columns. sad but true. Usually it's the header lines.
+                # This might be subsections of the table.
+                continue
+            # The first cell in a row contains a lot of whitespaces and somewhere in between a name.
+            # e.g. "   INVOIC / REMADV AHB 2.4 Konsolidierte Lesefassung mit Fehlerkorrekturen Stand: 01.07.2020    "
+            # To normalize it, we remove all adjacent occurences of more than 1 whitespaces and replace characters that
+            # might cause problems in filenames (e.g. slash)
+            # Looking back, this might not be the most readable format to store the files but by keeping it, it's way
+            # easier to keep track of a file based history in our git archive.
+            doc_name = (
+                re.sub(r"\s{2,}", "", table_cells[0].text)
+                .replace(":", "")
+                .replace(" ", "")
+                .replace("/", "")
+            )
+            # the "Gültig ab" column / publication date is the second column. e.g. "    17.12.2019    "
+            # Spoiler: It's not the real publication date. They modify the files once in a while without updating it.
+            publication_date = datetime.datetime.strptime(
+                table_cells[1].text.strip(), "%d.%m.%Y"
+            )
+            try:
+                # the "Gültig bis" column / valid to date describes on which date the document becomes legally binding.
+                # usually this is something like "   31.03.2020   " or "30.09.2019"
+                valid_to_date = datetime.datetime.strptime(
+                    table_cells[2].text.strip(), "%d.%m.%Y"
+                )
+            except ValueError as value_error:
+                # there's a special case: "Offen" means the document is valid until further notice.
+                if table_cells[2].text.strip() == "Offen":
+                    valid_to_date = datetime.date(9999, 12, 31)
+                else:
+                    raise value_error
+            # the 4th column contains a download link for the PDF.
+            file_link = table_cells[3].find("a").attrs["href"]
+            # there was a bug until 2021-02-10 where I used a weird %Y%d%m instead of %Y%m%d format.
+            file_name = f"{doc_name}_{valid_to_date.strftime('%Y%m%d')}_{publication_date.strftime('%Y%m%d')}.pdf"
+            result[file_name] = file_link
+        return result
+
+    def mirror(self):
+        """
+        Main method of the scraper. Downloads all the files and pages and stores them in the filesystem
+        """
+        index_soup = self.get_index()
+        index_path: Path = Path(self._root_dir, "index.html")
+        with open(index_path, "w+", encoding="utf8") as outfile:
+            # save the index file as html
+            outfile.write(index_soup.prettify())
+        epoch_links = EdiEnergyScraper.get_epoch_links(
+            self._get_soup(self.get_documents_page_link(index_soup))
+        )
+        for epoch, epoch_link in epoch_links.items():
+            epoch_soup = self._get_soup(epoch_link)
+            epoch_path: Path = Path(
+                self._root_dir, f"{epoch}.html"
+            )  # e.g. "future.html"
+            with open(epoch_path, "w+", encoding="utf8") as outfile:
+                outfile.write(epoch_soup.prettify())
+            file_map = EdiEnergyScraper.get_epoch_file_map(epoch_soup)
+            for file_name, link in file_map.items():
+                self._download_and_save_pdf(file_name=file_name, link=link, epoch=epoch)
---
+++ unittests/test_edienergyscraper.py
@@ -0,0 +1,265 @@
+import time
+from pathlib import Path
+
+import pytest
+from bs4 import BeautifulSoup
+from edienergyscraper import EdiEnergyScraper, Epoch
+
+
+def fast_waiter():
+    """
+    A helper method to prevent actual DOS waiting (up to 3 seconds per call.)
+    """
+    time.sleep(0)
+
+
+class TestEdiEnergyScraper:
+    """
+    A class to test the EdiEnergyScraper.
+    """
+
+    def test_instantiation(self):
+        """
+        Tests, that the constructor works.
+        """
+        instance = EdiEnergyScraper(root_url="https://my_url.de/")
+        assert not instance._root_url.endswith("/")
+
+    @pytest.mark.datafiles(
+        "./unittests/testfiles/index_20210208.html",
+    )
+    def test_index_retrieval(self, requests_mock, datafiles):
+        """
+        Tests that the landing page is downloaded correctly
+        """
+        with open(datafiles / "index_20210208.html", "r", encoding="utf8") as infile:
+            response_body = infile.read()
+        assert (
+            "<!--" in response_body
+        )  # original response contains comments, will be removed
+        requests_mock.get("https://www.my_root_url.test", text=response_body)
+        ees = EdiEnergyScraper("https://www.my_root_url.test", dos_waiter=fast_waiter)
+        actual_soup = ees.get_index()
+        actual_html = actual_soup.prettify()
+        assert "<!--" not in actual_html, "comments should be ignored/removed"
+        assert (
+            "Startseite: BDEW Forum Datenformate" in actual_html
+        ), "content should be returned"
+
+    @pytest.mark.datafiles(
+        "./unittests/testfiles/index_20210208.html",
+    )
+    def test_dokumente_link(self, requests_mock, datafiles):
+        """
+        Tests that the "Dokumente" link is extracted from the downloaded landing page.
+        """
+        with open(datafiles / "index_20210208.html", "r", encoding="utf8") as infile:
+            response_body = infile.read()
+        requests_mock.get("https://www.edi-energy.de", text=response_body)
+        ees = EdiEnergyScraper("https://www.edi-energy.de", dos_waiter=fast_waiter)
+        actual_link = ees.get_documents_page_link(ees.get_index())
+        assert actual_link == "https://www.edi-energy.de/index.php?id=38"
+
+    @pytest.mark.datafiles(
+        "./unittests/testfiles/dokumente_20210208.html",
+    )
+    def test_epoch_links_extraction(self, datafiles):
+        """
+        Tests that the links to past/current/future documents overview pages are extracted.
+        """
+        with open(
+            datafiles / "dokumente_20210208.html", "r", encoding="utf8"
+        ) as infile:
+            response_body = infile.read()
+        soup = BeautifulSoup(response_body, "html.parser")
+        actual = EdiEnergyScraper.get_epoch_links(soup)
+        assert len(actual.keys()) == 3
+        assert (
+            actual[Epoch.CURRENT]
+            == "https://www.edi-energy.de/index.php?id=38&tx_bdew_bdew%5Bview%5D=now&tx_bdew_bdew%5Baction%5D=list&tx_bdew_bdew%5Bcontroller%5D=Dokument&cHash=5d1142e54d8f3a1913af8e4cc56c71b2"
+        )
+        assert (
+            actual[Epoch.PAST]
+            == "https://www.edi-energy.de/index.php?id=38&tx_bdew_bdew%5Bview%5D=archive&tx_bdew_bdew%5Baction%5D=list&tx_bdew_bdew%5Bcontroller%5D=Dokument&cHash=6dd9d237ef46f6eebe2f4ef385528382"
+        )
+        assert (
+            actual[Epoch.FUTURE]
+            == "https://www.edi-energy.de/index.php?id=38&tx_bdew_bdew%5Bview%5D=future&tx_bdew_bdew%5Baction%5D=list&tx_bdew_bdew%5Bcontroller%5D=Dokument&cHash=325de212fe24061e83e018a2223e6185"
+        )
+
+    @pytest.mark.datafiles(
+        "./unittests/testfiles/future_20210210.html",
+    )
+    def test_epoch_file_map_future_20210210(self, datafiles):
+        with open(datafiles / "future_20210210.html", "r", encoding="utf8") as infile:
+            response_body = infile.read()
+        soup = BeautifulSoup(response_body, "html.parser")
+        actual = EdiEnergyScraper.get_epoch_file_map(soup)
+        assert len(actual.keys()) == 76
+        for file_name in actual.keys():
+            # all the future names should contain 99991231 as "valid to" date
+            assert "_99991231_" in file_name
+        assert (
+            actual[
+                "UTILMDAHBWiM3.1bKonsolidierteLesefassungmitFehlerkorrekturenStand18.12.2020_99991231_20210401.pdf"
+            ]
+            == "https://www.edi-energy.de/index.php?id=38&tx_bdew_bdew%5Buid%5D=1000&tx_bdew_bdew%5Baction%5D=download&tx_bdew_bdew%5Bcontroller%5D=Dokument&cHash=dbf7d932028aa2059c96b25a684d02ed"
+        )
+
+    @pytest.mark.datafiles(
+        "./unittests/testfiles/current_20210210.html",
+    )
+    def test_epoch_file_map_current_20210210(self, datafiles):
+        with open(datafiles / "current_20210210.html", "r", encoding="utf8") as infile:
+            response_body = infile.read()
+        soup = BeautifulSoup(response_body, "html.parser")
+        actual = EdiEnergyScraper.get_epoch_file_map(soup)
+        assert len(actual.keys()) == 81
+        for file_name in actual.keys():
+            # all the current documents are either "open" or valid until April 2021
+            assert "_99991231_" in file_name or "_20210331_" in file_name
+        assert (
+            actual[
+                "QUOTESMIG1.1aKonsolidierteLesefassungmitFehlerkorrekturenStand15.07.2019_20210331_20191201.pdf"
+            ]
+            == "https://www.edi-energy.de/index.php?id=38&tx_bdew_bdew%5Buid%5D=738&tx_bdew_bdew%5Baction%5D=download&tx_bdew_bdew%5Bcontroller%5D=Dokument&cHash=f01ed973e9947ccf6b91181c93cd2a28"
+        )
+
+    @pytest.mark.datafiles(
+        "./unittests/testfiles/past_20210210.html",
+    )
+    def test_epoch_file_map_past_20210210(self, datafiles):
+        with open(datafiles / "past_20210210.html", "r", encoding="utf8") as infile:
+            response_body = infile.read()
+        soup = BeautifulSoup(response_body, "html.parser")
+        actual = EdiEnergyScraper.get_epoch_file_map(soup)
+        assert len(actual.keys()) == 705
+
+    @pytest.mark.datafiles(
+        "./unittests/testfiles/example_ahb.pdf",
+    )
+    def test_pdf_download(self, requests_mock, tmpdir_factory, datafiles):
+        """
+        Tests that a PDF can be downloaded and is stored.
+        """
+        ees_dir = tmpdir_factory.mktemp("test_dir")
+        ees_dir.mkdir("future")
+        with open(datafiles / "example_ahb.pdf", "rb") as pdf_file:
+            # Note that we do _not_ use pdf_file.read() here but provide the requests_mocker with a file handle.
+            # Otherwise you'd run into a "ValueError: Unable to determine whether fp is closed."
+            # docs: https://requests-mock.readthedocs.io/en/latest/response.html?highlight=file#registering-responses
+            requests_mock.get("https://my_file_link.inv/foo_bar.pdf", body=pdf_file)
+            ees = EdiEnergyScraper(
+                "https://my_file_link.inv/",
+                dos_waiter=fast_waiter,
+                path_to_mirror_directory=ees_dir,
+            )
+            ees._download_and_save_pdf(
+                epoch=Epoch.FUTURE, file_name="my_favourite_ahb.pdf", link="foo_bar.pdf"
+            )
+        assert ees_dir.join(Path("future/my_favourite_ahb.pdf")).exists()
+
+    @staticmethod
+    def _get_soup_mocker(*args, **kwargs):
+        if args[0] == "current.html":
+            with open(
+                "./unittests/testfiles/current_20210210.html", "r", encoding="utf8"
+            ) as infile_current:
+                response_body = infile_current.read()
+        elif args[0] == "past.html":
+            with open(
+                "./unittests/testfiles/past_20210210.html", "r", encoding="utf8"
+            ) as infile_past:
+                response_body = infile_past.read()
+        elif args[0] == "future.html":
+            with open(
+                "./unittests/testfiles/future_20210210.html", "r", encoding="utf8"
+            ) as infile_future:
+                response_body = infile_future.read()
+        elif args[0] == "https://www.edi-energy.de":
+            with open(
+                "./unittests/testfiles/index_20210208.html", "r", encoding="utf8"
+            ) as infile_index:
+                response_body = infile_index.read()
+        elif args[0] == "https://www.edi-energy.de/index.php?id=38":
+            with open(
+                "./unittests/testfiles/dokumente_20210208.html", "r", encoding="utf8"
+            ) as infile_docs:
+                response_body = infile_docs.read()
+        else:
+            raise NotImplementedError(
+                f"The soup for {args[0]} is not implemented in this test."
+            )
+        soup = BeautifulSoup(response_body, "html.parser")
+        return soup
+
+    @staticmethod
+    def _get_efm_mocker(*args, **kwargs):
+        heading = args[0].find("h2").text
+        if heading == "Aktuell gültige Dokumente":
+            return {"xyz.pdf": "/a_current_ahb.pdf"}
+        if heading == "Zukünftige Dokumente":
+            return {"def.pdf": "/a_future_ahb.pdf"}
+        if heading == "Archivierte Dokumente":
+            return {"abc.pdf": "/a_past_ahb.pdf"}
+        raise NotImplementedError(
+            f"The case '{heading}' is not implemented in this test."
+        )
+
+    @pytest.mark.datafiles(
+        "./unittests/testfiles/example_ahb.pdf",
+        "./unittests/testfiles/dokumente_20210208.html",
+        "./unittests/testfiles/index_20210208.html",
+        "./unittests/testfiles/current_20210210.html",
+        "./unittests/testfiles/past_20210210.html",
+        "./unittests/testfiles/future_20210210.html",
+    )
+    def test_mirroring(self, mocker, requests_mock, tmpdir_factory, datafiles):
+        """
+        Tests the overall process and mocks most of the already tested methods.
+        """
+        ees_dir = tmpdir_factory.mktemp("test_dir_mirror")
+        ees_dir.mkdir("future")
+        ees_dir.mkdir("current")
+        ees_dir.mkdir("past")
+        with open(datafiles / "example_ahb.pdf", "rb") as pdf_file_current, open(
+            datafiles / "example_ahb.pdf", "rb"
+        ) as pdf_file_future, open(
+            datafiles / "example_ahb.pdf", "rb"
+        ) as pdf_file_past:
+            requests_mock.get(
+                "https://www.edi-energy.de/a_future_ahb.pdf", body=pdf_file_future
+            )
+            requests_mock.get(
+                "https://www.edi-energy.de/a_current_ahb.pdf", body=pdf_file_current
+            )
+            requests_mock.get(
+                "https://www.edi-energy.de/a_past_ahb.pdf", body=pdf_file_past
+            )
+            mocker.patch(
+                "edienergyscraper.EdiEnergyScraper.get_epoch_links",
+                return_value={
+                    "current": "current.html",
+                    "future": "future.html",
+                    "past": "past.html",
+                },
+            )
+            mocker.patch(
+                "edienergyscraper.EdiEnergyScraper._get_soup",
+                side_effect=TestEdiEnergyScraper._get_soup_mocker,
+            )
+            mocker.patch(
+                "edienergyscraper.EdiEnergyScraper.get_epoch_file_map",
+                side_effect=TestEdiEnergyScraper._get_efm_mocker,
+            )
+            ees = EdiEnergyScraper(
+                dos_waiter=fast_waiter, path_to_mirror_directory=ees_dir
+            )
+            ees.mirror()
+        assert ees_dir.join(Path("index.html")).exists()
+        assert ees_dir.join(Path("future.html")).exists()
+        assert ees_dir.join(Path("current.html")).exists()
+        assert ees_dir.join(Path("past.html")).exists()
+        assert ees_dir.join(Path("future")).join(Path("def.pdf")).exists()
+        assert ees_dir.join(Path("past")).join(Path("abc.pdf")).exists()
+        assert ees_dir.join(Path("current")).join(Path("xyz.pdf")).exists()
