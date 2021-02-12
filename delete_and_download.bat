52a43de425c73031dd399dde699dd7b1d893fe4a
Annika Schlögl <annika.schloegl@hochfrequenz.de>
2021-02-12 19:11:36 +0100

    🔥 Remove delete commands from batch
    Function is now included in download_and_post_process.py

--- delete_and_download.bat
+++ delete_and_download.bat
@@ -1,13 +1,6 @@
 C:
 cd C:/Users/Administrator/Documents/edi_energy_mirror/
 git pull
-cd edi_energy_de/future
-del *.pdf
-cd ../current/
-del *.pdf
-cd ../past/
-del *.pdf
-cd ../../
 python download_and_post_process.py
 git add edi_energy_de/*.html
 git add --no-ignore-removal edi_energy_de/future/*.pdf
