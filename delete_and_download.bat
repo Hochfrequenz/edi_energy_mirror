ee0ec8a09eb51b393ac2e56bd919b7df29caf828
Annika Schlögl <annika.schloegl@hochfrequenz.de>
2021-02-18 10:46:39 +0100

    🐛 Enable all file types to be tracked by git

--- delete_and_download.bat
+++ delete_and_download.bat
@@ -3,8 +3,8 @@ cd C:/Users/Administrator/Documents/edi_energy_mirror/
 git pull
 python download_and_post_process.py
 git add edi_energy_de/*.html
-git add --no-ignore-removal edi_energy_de/future/*.pdf
-git add --no-ignore-removal edi_energy_de/current/*.pdf
-git add --no-ignore-removal edi_energy_de/past/*.pdf
+git add --no-ignore-removal edi_energy_de/future/*
+git add --no-ignore-removal edi_energy_de/current/*
+git add --no-ignore-removal edi_energy_de/past/*
 git commit -m "edi energy mirror cronjob"
 git push
