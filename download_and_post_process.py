7f69fbfb18320b0edcd7cf83f09b57d18538a91a
konstantin <konstantin.klein@hochfrequenz.de>
2023-01-23 08:03:56 +0100

    Update mirror.yml and download_and_post_process.py
    Update mirror.yml
    Update download_and_post_process.py
    Update download_and_post_process.py
    Update mirror.yml
    Update mirror.yml
    Update mirror.yml
    Update mirror.yml
    Update mirror.yml

--- download_and_post_process.py
+++ download_and_post_process.py
@@ -1,4 +1,5 @@
+from time import sleep
 from edi_energy_scraper import EdiEnergyScraper
 
-ees = EdiEnergyScraper(dos_waiter=lambda: sleep(0))
+ees = EdiEnergyScraper(path_to_mirror_directory="edi_energy_de", dos_waiter=lambda: sleep(0))
 ees.mirror()
