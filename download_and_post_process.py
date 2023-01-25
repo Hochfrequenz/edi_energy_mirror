b8b8d3ceac51efae3b97c75f60647a38e52f7c1e
konstantin <konstantin.klein@hochfrequenz.de>
2023-01-25 08:11:33 +0100

    Delete download_and_post_process.py

--- download_and_post_process.py
+++
@@ -1,5 +0,0 @@
-from time import sleep
-from edi_energy_scraper import EdiEnergyScraper
-
-ees = EdiEnergyScraper(path_to_mirror_directory="edi_energy_de", dos_waiter=lambda: sleep(0))
-ees.mirror()
