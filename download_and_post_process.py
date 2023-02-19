7c7d223f9d5a30c52e8ce9703667a8fe343ea6d7
konstantin <konstantin.klein@hochfrequenz.de>
2023-02-19 22:40:24 +0100

    Update download_and_post_process.py

--- download_and_post_process.py
+++ download_and_post_process.py
@@ -1,5 +1,13 @@
-from time import sleep
+import asyncio
+
 from edi_energy_scraper import EdiEnergyScraper
 
-ees = EdiEnergyScraper(path_to_mirror_directory="edi_energy_de", dos_waiter=lambda: sleep(0))
-ees.mirror()
+
+async def mirror():
+    scraper = EdiEnergyScraper(path_to_mirror_directory="edi_energy_de")
+    await scraper.mirror()
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(mirror())
